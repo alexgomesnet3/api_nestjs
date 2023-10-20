20997beab7d1253cfe60c2c7f7ad4726587b55f5
Alex Gomes <alex.gomes.net3@gmail.com>
2023-10-20 16:31:28 -0300

    Created UserService

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,7 +1,10 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('users')
 export class UserController {
+  constructor(private userService: UserService) {}
+
   @Get()
   async getUsers() {
     return {
